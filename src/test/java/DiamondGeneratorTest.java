cb3822b82cd01a83223ab167e3cd030c0699922a
“sitajaf” <sitajaf@gmail.com>
2016-01-17 17:41:44 +1100

    some more clean up

--- src/test/java/DiamondGeneratorTest.java
+++ src/test/java/DiamondGeneratorTest.java
@@ -5,40 +5,40 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class DiamondGeneratorTest {
-    DiamondGenerator builder;
+    DiamondGenerator generator;
 
     @Before
     public void setUp() throws Exception {
-        builder = new DiamondGenerator();
+        generator = new DiamondGenerator();
     }
 
     @Test
     public void shouldReturnABDiamondString() throws Exception {
-        String diamond = builder.generate("B");
+        String diamond = generator.generate("B");
 
         assertThat(diamond, is(" A\nB B\n A\n"));
     }
 
     @Test
     public void shouldReturnABCDDiamondStringEvenWithSmallCaseInput() throws Exception {
-        String diamond = builder.generate("d");
+        String diamond = generator.generate("d");
 
         assertThat(diamond, is("   A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A\n"));
     }
 
     @Test
     public void shouldNotAllowInvalidInput() throws Exception {
-        String diamond = builder.generate("23");
+        String diamond = generator.generate("23");
         assertThat(diamond, is("Invalid Input!"));
     }
 
     @Test
     public void shouldNotAllowOtherInvalidCharacters() throws Exception {
-        assertThat(builder.generate("/"), is("Invalid Input!"));
-        assertThat(builder.generate("3"), is("Invalid Input!"));
-        assertThat(builder.generate("+"), is("Invalid Input!"));
-        assertThat(builder.generate(","), is("Invalid Input!"));
-        assertThat(builder.generate("JoJO"), is("Invalid Input!"));
+        assertThat(generator.generate("/"), is("Invalid Input!"));
+        assertThat(generator.generate("3"), is("Invalid Input!"));
+        assertThat(generator.generate("+"), is("Invalid Input!"));
+        assertThat(generator.generate(","), is("Invalid Input!"));
+        assertThat(generator.generate("JoJO"), is("Invalid Input!"));
     }
 
 }
